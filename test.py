0111a73764141eb68d75ed8a4b8490fca730021f
Sung-Lin Yeh <sunglin.yeh@ed.ac.uk>
2025-04-29 18:12:11 +0100

    add eval pipeline

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 import os
 import numpy as np
+from glob import glob
+from tqdm import tqdm
 import string
 import torch
+import torchaudio
+from scipy.ndimage import median_filter
+import matplotlib.pyplot as plt
+from metrics import eval_n1, get_seg_metrics
+
 import whisper
 from whisper.timing import dtw
 from whisper.tokenizer import get_tokenizer
 from whisper.audio import HOP_LENGTH, SAMPLE_RATE, TOKENS_PER_SECOND
-from scipy.ndimage import median_filter
-import matplotlib.pyplot as plt
 
 
 def coverage_penalty(attn, threshold=0.5):
@@ -82,8 +87,7 @@ def get_attentions(mel, tokens, model, tokenizer, medfilt_width=7, qk_scale=1.0)
         logits = model(mel.unsqueeze(0), tokens.unsqueeze(0))[0]
 
     weights = torch.cat(QKs)  # layers * heads * tokens * frames    
-    weights = weights[:, :, len(tokenizer.sot_sequence):-1, 
-                      :duration // AUDIO_SAMPLES_PER_TOKEN].cpu()
+    weights = weights.cpu()
     weights = median_filter(weights, (1, 1, 1, medfilt_width))
     weights = torch.tensor(weights * qk_scale).softmax(dim=-1)
     w = weights / weights.norm(dim=-2, keepdim=True)
@@ -92,6 +96,7 @@ def get_attentions(mel, tokens, model, tokenizer, medfilt_width=7, qk_scale=1.0)
 def force_align(
         w, tokens, 
         tokenizer, 
+        max_frames,
         aggregation="mean", 
         topk=-1, 
         plot=False,
@@ -112,6 +117,7 @@ def force_align(
         matrix = filter_attention(w, topk=topk, plot=plot, path=path, wrd_pos=wrd_pos)
         matrix = torch.cat(matrix, 0).mean(0)
 
+    matrix = matrix[len(tokenizer.sot_sequence):-1, :max_frames].cpu()
     text_indices, time_indices = dtw(-matrix)
 
     words, word_tokens = tokenizer.split_to_word_tokens(tokens + [tokenizer.eot])
@@ -131,6 +137,100 @@ def force_align(
     return words, start_times, end_times
 
 
+class TIMIT(torch.utils.data.Dataset):
+    def __init__(self, scp_file="scp/test.wav.scp", split="test", n_mels=80, device='cpu:0'):
+        self.sample_rate = 16000
+        self.dataset = []
+        scp = open(scp_file, 'r').readlines()
+        for line in scp:
+            splits = line.split()
+            fid = splits[0]
+            # audio
+            audio_file = splits[1]
+            audio, sample_rate = torchaudio.load(audio_file)
+            audio = audio.squeeze()
+            # text
+            text_file = audio_file.split('.wav')[0] + '.wrd'
+            text, starts, ends = self.process_text(text_file)
+            self.dataset.append((audio, sample_rate, text, starts, ends, fid))
+        self.n_mels = n_mels
+        self.device = device
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, item):
+        audio, sample_rate, text, starts, ends, fid = self.dataset[item]
+        assert sample_rate == self.sample_rate
+        duration = len(audio.flatten())
+        audio = whisper.pad_or_trim(audio.flatten())
+        mel = whisper.log_mel_spectrogram(audio, self.n_mels)
+        mel = mel.to(self.device)
+
+        return mel, duration, text, starts, ends, fid
+
+    def process_text(self, filename):
+        starts = []
+        ends = []
+        texts = []
+        f = open(filename, 'r')
+        for line in f.readlines():
+            splits = line.split()
+            starts.append(float(splits[0])/self.sample_rate)
+            ends.append(float(splits[1])/self.sample_rate)
+            texts.append(splits[2])
+        texts = " ".join(texts)
+        return texts, starts, ends
+
+class Collate: 
+    def __call__(self, batch):
+        one_batch = list(zip(*batch))
+        mel, duration, text, starts, ends, fid = one_batch
+        return  mel[0], duration[0], text[0], starts[0], ends[0], fid[0]
+
+def infer_dataset(model, tokenizer, scp_file="scp/test.wav.scp", tolerance=0.02):
+    dataset = TIMIT(scp_file, n_mels=80, device=model.device)
+    loader = torch.utils.data.DataLoader(dataset, collate_fn=Collate(), batch_size=1)
+
+    # decode the audio
+    options = whisper.DecodingOptions(language="en")
+
+    corrects = 0
+    total_preds = 0
+    total_gts = 0
+    for n, (mels, durations, texts, starts, ends, fids) in enumerate(tqdm(loader)):
+        # print the recognized text
+        result = whisper.decode(model, mels, options)
+        transcription = result.text
+        transcription = transcription.translate(str.maketrans('', '', string.punctuation))
+
+        text_tokens = tokenizer.encode(transcription)
+        tokens = torch.tensor(
+            [
+                *tokenizer.sot_sequence,
+                tokenizer.no_timestamps,
+                *text_tokens,
+                tokenizer.eot,
+            ]
+        ).to(model.device)
+
+        # Get attention maps
+        max_frames = durations // AUDIO_SAMPLES_PER_TOKEN
+        w, logits = get_attentions(mels, tokens, model, tokenizer, medfilt_width, qk_scale)
+        results = force_align(w, text_tokens, tokenizer, max_frames, aggregation="topk", topk=50, plot=False, wrd_pos=ends)
+
+        # predicted boundaries
+        ends_hat = results[2]
+
+        # eval
+        total_gts += len(ends)
+        total_preds += len(ends_hat)
+        correct_pred, _ = eval_n1(ends, ends_hat, tolerance)
+        corrects += correct_pred
+    precision, recall, f1, r_value, os = \
+             get_seg_metrics(corrects, corrects, total_preds, total_gts)
+    print(precision, recall, f1, r_value)
+
 
 
 AUDIO_SAMPLES_PER_TOKEN = whisper.audio.HOP_LENGTH * 2
@@ -144,51 +244,7 @@ qk_scale = 1.0
 model = whisper.load_model("medium")
 
 # decode the audio
-options = whisper.DecodingOptions()
+options = whisper.DecodingOptions(language="en")
 
-
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("/home/s2196654/dataset/timit/timit-wav/train/dr1/fcjf0/sa1.wav")
-duration = len(audio)
-audio = whisper.pad_or_trim(audio)
-
-# audio
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
-
-# print the recognized text
-result = whisper.decode(model, mel, options)
-print(result.text)
-
-# tokens
 tokenizer = get_tokenizer(model.is_multilingual, language='English')
-transcription = result.text
-transcription = transcription.translate(str.maketrans('', '', string.punctuation))
-#transcription = ' '.join([i for i in transcription])
-
-wrd_pos = []
-f = open("/home/s2196654/dataset/timit/timit-wav/train/dr1/fcjf0/sa1.wrd", 'r')
-for line in f.readlines():
-    wrd_pos.append(int(line.split()[1])/SAMPLE_RATE)
-
-text_tokens = tokenizer.encode(transcription)
-tokens = torch.tensor(
-    [
-        *tokenizer.sot_sequence,
-        tokenizer.no_timestamps,
-        *text_tokens,
-        tokenizer.eot,
-    ]
-).to(model.device)
-print(tokens)
-
-# Get attention maps
-print(wrd_pos)
-wrd_pos = [int(i/0.02) for i in wrd_pos]
-w, logits = get_attentions(mel, tokens, model, tokenizer, medfilt_width, qk_scale)
-results = force_align(w, text_tokens, tokenizer, aggregation="mean", topk=20, plot=False, wrd_pos=wrd_pos)
-import pdb; pdb.set_trace()
+infer_dataset(model, tokenizer)
