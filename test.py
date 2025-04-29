05f04172f82a23318c3011a6e9ee9d82d95a0909
Sung-Lin Yeh <sunglin.yeh@ed.ac.uk>
2025-04-29 10:20:59 +0100

    refactor code

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
+import os
+import numpy as np
+import string
 import torch
 import whisper
+from whisper.timing import dtw
 from whisper.tokenizer import get_tokenizer
+from whisper.audio import HOP_LENGTH, SAMPLE_RATE, TOKENS_PER_SECOND
 from scipy.ndimage import median_filter
 import matplotlib.pyplot as plt
+
 
 def coverage_penalty(attn, threshold=0.5):
     """
@@ -29,103 +35,160 @@ def count_transitions(x):
 
     return count, positions
 
+def filter_attention(w, topk=20, plot=True, path="imgs", wrd_pos=None):
+    """
+    w : torch.tensor in (layers, heads, tokens, frames)
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+    scores = []
+    for l in range(w.size(0)):
+        for n_h in range(w.size(1)):
+            score = coverage_penalty(w[l, n_h])
+            name = f"sample_layer{l}_head{n_h}"
+            scores.append((score, (l, n_h), name))
+
+    scores_sorted = sorted(scores)[:topk]
+    ws = []
+    for _, (l, n_h), name in scores_sorted:
+
+        name = f"sample_layer{l}_head{n_h}"
+        ws.append(w[l, n_h].unsqueeze(0))
+        if plot:
+            assert wrd_pos is not None
+            plt.vlines(wrd_pos, ymin=-1, ymax=w[l, n_h].size(0), colors="white")
+            plt.imshow(w[l, n_h], aspect="auto")
+            plt.savefig(f"{path}/{name}.png")
+
+    if plot:
+        ws_ = torch.cat(ws, 0).mean(0)
+        plt.vlines(wrd_pos, ymin=-1, ymax=w[l, n_h].size(0), colors="white")
+        plt.imshow(ws_, aspect="auto")
+        plt.savefig(f"{path}/sample_ave.png")
+    
+    return ws
+
+def get_attentions(mel, tokens, model, tokenizer, medfilt_width=7, qk_scale=1.0):
+    # install hooks on the cross attention layers to retrieve the attention weights
+    # NOTE: make sure MultiHeadAttention.use_sdpa = False
+    QKs = [None] * model.dims.n_text_layer
+
+    for i, block in enumerate(model.decoder.blocks):
+        block.cross_attn.register_forward_hook(
+            lambda _, ins, outs, index=i: QKs.__setitem__(index, outs[-1])
+        )
+
+    with torch.no_grad():
+        logits = model(mel.unsqueeze(0), tokens.unsqueeze(0))[0]
+
+    weights = torch.cat(QKs)  # layers * heads * tokens * frames    
+    weights = weights[:, :, len(tokenizer.sot_sequence):-1, 
+                      :duration // AUDIO_SAMPLES_PER_TOKEN].cpu()
+    weights = median_filter(weights, (1, 1, 1, medfilt_width))
+    weights = torch.tensor(weights * qk_scale).softmax(dim=-1)
+    w = weights / weights.norm(dim=-2, keepdim=True)
+    return w, logits
+
+def force_align(
+        w, tokens, 
+        tokenizer, 
+        aggregation="mean", 
+        topk=-1, 
+        plot=False,
+        path="img_topk", 
+        wrd_pos=None
+    ):
+    """
+    w : torch.tensor in (layers, heads, tokens, frames)
+    tokens : tokens of texts, without bot, eot
+    """
+    if aggregation == "mean":
+        # whisper implementation:
+        matrix = w.mean(axis=(0, 1))
+
+    elif aggregation == "topk":
+        assert topk > 0
+        # select attentions:
+        matrix = filter_attention(w, topk=topk, plot=plot, path=path, wrd_pos=wrd_pos)
+        matrix = torch.cat(matrix, 0).mean(0)
+
+    text_indices, time_indices = dtw(-matrix)
+
+    words, word_tokens = tokenizer.split_to_word_tokens(tokens + [tokenizer.eot])
+    if len(word_tokens) <= 1:
+        # return on eot only
+        # >>> np.pad([], (1, 0))
+        # array([0.])
+        # This results in crashes when we lookup jump_times with float, like
+        # IndexError: arrays used as indices must be of integer (or boolean) type
+        return []
+    word_boundaries = np.pad(np.cumsum([len(t) for t in word_tokens[:-1]]), (1, 0))
+
+    jumps = np.pad(np.diff(text_indices), (1, 0), constant_values=1).astype(bool)
+    jump_times = time_indices[jumps] / TOKENS_PER_SECOND
+    start_times = jump_times[word_boundaries[:-1]]
+    end_times = jump_times[word_boundaries[1:]]
+    return words, start_times, end_times
+
+
+
 
 AUDIO_SAMPLES_PER_TOKEN = whisper.audio.HOP_LENGTH * 2
 AUDIO_TIME_PER_TOKEN = AUDIO_SAMPLES_PER_TOKEN / whisper.audio.SAMPLE_RATE
+print(TOKENS_PER_SECOND)
 
 # basically paremeters to do denoising
 medfilt_width = 7
 qk_scale = 1.0
 
 model = whisper.load_model("medium")
-tokenizer = get_tokenizer(model.is_multilingual, language='English')
+
+# decode the audio
+options = whisper.DecodingOptions()
 
 
 # load audio and pad/trim it to fit 30 seconds
 audio = whisper.load_audio("/home/s2196654/dataset/timit/timit-wav/train/dr1/fcjf0/sa1.wav")
 duration = len(audio)
 audio = whisper.pad_or_trim(audio)
-transcription = "She had your dark suit in greasy wash water all year"
 
 # audio
 mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-# tokens
-
-#transcription = ' '.join([i for i in transcription])
-tokens = torch.tensor(
-    [
-        *tokenizer.sot_sequence,
-        tokenizer.timestamp_begin,
-    ] + tokenizer.encode(transcription) + [
-        tokenizer.timestamp_begin + duration // AUDIO_SAMPLES_PER_TOKEN,
-        tokenizer.eot,
-    ]
-).cuda()
-phns = "sil sil sil sil sil sil sil sil sil sil sil sil sil sil sil sil sil sil sil sh sh sh sh sh sh sh sh sh ih ih ih ih ih ih ih hh hh hh hh hh hh eh eh eh eh eh eh eh eh eh eh eh eh eh sil sil sil jh jh jh jh jh jh jh ih ih ih ih ih ih ih sil sil sil sil sil sil sil d ah ah ah ah ah ah ah ah ah ah ah ah sil sil sil sil sil sil sil sil k k k s s s s s s s s s s s uw uw uw uw uw uw uw uw uw uw uw uw uw uw unk unk unk unk unk n n n n n n n n n sil sil g g r r r r r r ih ih ih ih ih ih ih ih s s s s s s s s s s ih ih ih ih ih ih w w w w w w w w w w aa aa aa aa aa aa aa aa aa aa aa aa aa sh sh sh sh sh sh sh sh sh sh sil sil w w w w w w aa aa aa aa aa aa aa aa aa dx dx er er er er er er er aa aa aa aa aa aa aa aa aa aa aa aa aa l l l l y y y y y y y y y y y ih ih ih ih ih ih ih ih ih er er er er er er er sil sil sil sil sil sil sil sil sil sil sil sil sil sil".split()[::2]
-
-_, pos = count_transitions(phns)
-wrd_pos = [10, 18, 27, 29, 36, 39, 51, 64, 71, 72, 88, 104, 105, 117, 126, 139, 139]
 
 
 # detect the spoken language
 _, probs = model.detect_language(mel)
 print(f"Detected language: {max(probs, key=probs.get)}")
 
-
-# install hooks on the cross attention layers to retrieve the attention weights
-# NOTE: make sure MultiHeadAttention.use_sdpa = False
-QKs = [None] * model.dims.n_text_layer
-
-for i, block in enumerate(model.decoder.blocks):
-    block.cross_attn.register_forward_hook(
-        lambda _, ins, outs, index=i: QKs.__setitem__(index, outs[-1])
-    )
-
-with torch.no_grad():
-    logits = model(mel.unsqueeze(0), tokens.unsqueeze(0))
-
-
-weights = torch.cat(QKs)  # layers * heads * tokens * frames    
-weights = weights[:, :, :, : duration // AUDIO_SAMPLES_PER_TOKEN].cpu()
-weights = median_filter(weights, (1, 1, 1, medfilt_width))
-weights = torch.tensor(weights * qk_scale).softmax(dim=-1)
-w = weights / weights.norm(dim=-2, keepdim=True)
-
-# whisper implementation:
-matrix = w[-6:].mean(axis=(0, 1))
-plt.imshow(matrix, aspect="auto")
-plt.savefig(f"imgs/sample_ave.png")
-
-
-scores = []
-for l in range(w.size(0)):
-    for n_h in range(w.size(1)):
-        score = coverage_penalty(w[l, n_h])
-        name = f"sample_layer{l}_head{n_h}"
-        print(name, score)
-        scores.append((score, (l, n_h), name))
-
-topk = 20
-scores_sorted = sorted(scores)[:topk]
-import pdb; pdb.set_trace()
-ws = []
-for _, (l, n_h), name in scores_sorted:
-    plt.vlines(wrd_pos, ymin=-1, ymax=w[l, n_h].size(0), colors="white")
-    plt.imshow(w[l, n_h], aspect="auto")
-    # y_max = # tokens
-    name = f"sample_layer{l}_head{n_h}"
-    plt.savefig(f"imgs_char_topk/{name}.png")
-    ws.append(w[l, n_h].unsqueeze(0))
-
-ws = torch.cat(ws, 0).mean(0)
-plt.vlines(wrd_pos, ymin=-1, ymax=w[l, n_h].size(0), colors="white")
-plt.imshow(ws, aspect="auto")
-plt.savefig(f"imgs_char_topk/sample_ave.png")
-
-# decode the audio
-options = whisper.DecodingOptions()
-result = whisper.decode(model, mel, options)
-
 # print the recognized text
+result = whisper.decode(model, mel, options)
 print(result.text)
+
+# tokens
+tokenizer = get_tokenizer(model.is_multilingual, language='English')
+transcription = result.text
+transcription = transcription.translate(str.maketrans('', '', string.punctuation))
+#transcription = ' '.join([i for i in transcription])
+
+wrd_pos = []
+f = open("/home/s2196654/dataset/timit/timit-wav/train/dr1/fcjf0/sa1.wrd", 'r')
+for line in f.readlines():
+    wrd_pos.append(int(line.split()[1])/SAMPLE_RATE)
+
+text_tokens = tokenizer.encode(transcription)
+tokens = torch.tensor(
+    [
+        *tokenizer.sot_sequence,
+        tokenizer.no_timestamps,
+        *text_tokens,
+        tokenizer.eot,
+    ]
+).to(model.device)
+print(tokens)
+
+# Get attention maps
+print(wrd_pos)
+wrd_pos = [int(i/0.02) for i in wrd_pos]
+w, logits = get_attentions(mel, tokens, model, tokenizer, medfilt_width, qk_scale)
+results = force_align(w, text_tokens, tokenizer, aggregation="mean", topk=20, plot=False, wrd_pos=wrd_pos)
+import pdb; pdb.set_trace()
