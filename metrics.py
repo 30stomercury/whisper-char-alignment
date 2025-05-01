4c7d4f68f895eeeabb7ad177938bb2405d153585
Sung-Lin Yeh <sunglin.yeh@ed.ac.uk>
2025-05-01 15:49:15 +0100

    add metrics

--- metrics.py
+++ metrics.py
@@ -59,3 +59,52 @@ class AlignmentMetric:
         self.gt_counter += gt_counter
 
         return self.get_metrics(precision_counter, recall_counter, pred_counter, gt_counter)
+
+
+def eval_n1(y, yhat, tolerance=1):
+    def is_match(i, j, tolerance):
+        return (1 if abs(i-j) <= tolerance else 0)
+    n_match = 0
+
+    if len(yhat) == 0:
+        return 0, 0
+
+    i, j = 0, 0
+    while i < len(y) and j < len(yhat):
+        if is_match(y[i], yhat[j], tolerance):
+            i += 1
+            j += 1
+            n_match += 1
+
+        elif y[i] < yhat[j]:
+            i += 1
+
+        elif y[i] > yhat[j]:
+            j += 1
+
+    return n_match, n_match
+
+def get_seg_metrics(correct_predict, correct_retrieve, total_predict, total_gold):
+    EPS = 1e-7
+    
+    precision = correct_predict / (total_predict + EPS)
+    recall = correct_retrieve / (total_gold + EPS)
+    f1 = 2 * (precision * recall) / (precision + recall + EPS)
+    
+    os = recall / (precision + EPS) - 1
+    r1 = math.sqrt((1 - recall) ** 2 + os ** 2)
+    r2 = (-os + recall - 1) / (math.sqrt(2))
+    r_value = 1 - (abs(r1) + abs(r2)) / 2
+
+    return precision, recall, f1, r_value, os
+
+def count_transitions(x):
+    count = 0
+    positions = []
+    prev = x[0]
+    for i in range(1, len(x)):
+        if x[i] != x[i-1]: 
+            positions.append(i)
+            count += 1
+
+    return count, positions
