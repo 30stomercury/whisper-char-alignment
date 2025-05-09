04b0050a8efd2b762497b4984b8d090fb5909d47
slyeh <slyeh@slyehs-Mac-mini.local>
2025-05-09 19:07:03 +0100

    ami init attempt

--- dataset.py
+++ dataset.py
@@ -117,3 +117,58 @@ class LibriSpeech(torch.utils.data.Dataset):
                 ends.append(item[2])
 
         return (audio, mel, duration, text, starts, ends, fid)
+
+
+
+
+class AMI(torch.utils.data.Dataset):
+    def __init__(self, scp_file="scp/test.wav.scp", n_mels=80, device='cpu:0'):
+        import xml.etree.ElementTree as ET
+        from datasets import load_dataset
+        ds = load_dataset("edinburghcstr/ami", "ihm")
+        x = ET.parse('/Users/slyeh/Downloads/ami_public_auto_1.5.1/ASR/ASR_AS_CTM_v1.0_feb07/EN2001a.B.words.xml')
+        root = x.getroot()
+        for child in root:
+            print(child.tag, child.attrib)
+        self.sample_rate = 16000
+        self.dataset = []
+        for line in scp:
+            splits = line.split()
+            fid = splits[0]
+            # audio
+            audio_file = splits[1]
+            audio, sample_rate = torchaudio.load(audio_file)
+            audio = audio.squeeze()
+            # text
+            text_file = audio_file.split('.wav')[0] + '.wrd'
+            text, starts, ends = self.process_text(text_file)
+            self.dataset.append((audio, sample_rate, text, starts, ends, fid))
+        self.n_mels = n_mels
+        self.device = device
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, item):
+        audio, sample_rate, text, starts, ends, fid = self.dataset[item]
+        assert sample_rate == self.sample_rate
+        duration = len(audio.flatten())
+        audio = whisper.pad_or_trim(audio.flatten())
+        mel = whisper.log_mel_spectrogram(audio, self.n_mels)
+        mel = mel.to(self.device)
+
+        return audio, mel, duration, text, starts, ends, fid
+
+    def process_text(self, filename):
+        starts = []
+        ends = []
+        texts = []
+        f = open(filename, 'r')
+        for line in f.readlines():
+            splits = line.split()
+            starts.append(float(splits[0])/self.sample_rate)
+            ends.append(float(splits[1])/self.sample_rate)
+            texts.append(splits[2])
+        texts = " ".join(texts)
+        return texts, starts, ends
+
